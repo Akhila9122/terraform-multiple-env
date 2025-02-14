ea6c174102913beb67eb63dbed1853e3da838968
Akhila9122 <akhilasaikam990@gmail.com>
2025-02-14 20:38:15 +0530

    terraform

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "daws9-dev"
+key    = "multi-env-tfvars"
+region = "us-east-1"
+dynamodb_table = "daws78s-dev"
---
+++ tfvars/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "expense" {
+    for_each = var.instance_names # each.key and each.value
+    ami = data.aws_ami.ami_info.id
+    vpc_security_group_ids = ["sg-0813c82c5c737b6d6"] # replace with you SG ID
+    instance_type = each.value
+    tags = merge(
+        var.common_tags,
+        {
+            Name = "${each.key}"
+            Module = "${each.key}"
+            Environment = var.environment
+        }
+    )
+}
